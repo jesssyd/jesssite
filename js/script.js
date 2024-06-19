174690b2d0a8cb92057a569b18af08026b8b55be
jesssyd <tidd.jessica.s@gmail.com>
2024-06-19 16:56:03 -0400

    Modal

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
     const navbar = document.querySelector('.navbar');
     const navbarToggler = document.querySelector('.custom-toggler');
-    const changePosition = 500;
+    let changePosition;
+
+    const navbarSectionId = navbar.closest('section').id;
+    let backgroundColor;
+
+    if (navbarSectionId === 'about-nav' || navbarSectionId === 'navigation') {
+        backgroundColor = 'rgba(235, 227, 213, 0.8)';
+        changePosition = 500;
+    } else if (navbarSectionId === 'photography-nav') {
+        backgroundColor = 'rgba(255, 255, 255, 0.8)';
+        changePosition = 25;
+    }
 
     function updateNavbarBackground() {
         const isExpanded = navbarToggler.getAttribute('aria-expanded') === 'true';
         if (window.scrollY >= changePosition || isExpanded) {
-            navbar.style.backgroundColor = 'rgba(235, 227, 213, 0.8)';
+            navbar.style.backgroundColor = backgroundColor;
         } else {
             navbar.style.backgroundColor = 'transparent';
         }
@@ -14,6 +25,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
     window.addEventListener('scroll', updateNavbarBackground);
     navbarToggler.addEventListener('click', function () {
-        setTimeout(updateNavbarBackground, 0); 
+        setTimeout(updateNavbarBackground, 0);
     });
 });
